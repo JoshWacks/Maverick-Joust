93fe9cc111f20d5a746cb2970a194f15c091cc5f
Johan Smit <jsjohansmit6@gmail.com>
2022-07-04 12:39:43 +0200

    Added GameService

---
+++ Backend/game/GameService.js
@@ -0,0 +1,17 @@
+class GameService {
+
+    /* Generate new game session and store in memory, return the session's id. */
+    create() {
+
+    }
+
+    /* Search for an available session by session id and increment the player count. */
+    join() {
+
+    }
+
+    /* Search for an available session by session id and decrement the player count.  */
+    lose() {
+
+    }
+}
